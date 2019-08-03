ff3f44ead2d5a674976094bb6fc6651fe39d3f87
Kuralamudhu <51950263+Kuralamudhu@users.noreply.github.com>
2019-08-03 09:57:56 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,6 +1,6 @@
 from itertools import combinations
 J,a=map(int,input().split())
 b=len(str(J))
-lst=list(combinations(str(J),b-a))
-lst=sorted(lst)
-print(*lst[0],dec='')
+list=list(combinations(str(J),b-a))
+list=sorted(list)
+print(*list[0],dec='')
